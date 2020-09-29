89ff67057ec26e395cc21e82fe3acf127b8b1e2e
Steve Alexander <steve.alexander@discotecnica.com>
2020-09-29 17:03:11 +0100

    Updates

---
+++ precache-manifest.d93c373cdedf22fd1d4f6ec652138526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb6d628e946b8b01cf7559d1ebc7558",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2211efb84b523bf99148",
+    "url": "/monsters-rolodex/static/css/main.d5608976.chunk.css"
+  },
+  {
+    "revision": "57595fc6caf45f548129",
+    "url": "/monsters-rolodex/static/js/2.c272e29f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c272e29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2211efb84b523bf99148",
+    "url": "/monsters-rolodex/static/js/main.898e955b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
